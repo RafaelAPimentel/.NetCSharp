d85cd0a3711b292e168bf46522764aee2ceb207c
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-01-09 23:05:29 -0500

    commiting

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        static long Add(long x, long y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  "Fun with Method Overloading");
+            //int Add method
+            Console.WriteLine(Add(10,10));
+            //double Add method
+            Console.WriteLine(Add(10.3,20.5));
+            //long Add method
+            Console.WriteLine(Add(90000000000000,9000000000));
+            Console.ReadLine();
+        }
+    }
+}
