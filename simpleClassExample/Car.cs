0cdafbef97ba89634b674ad3597f62ae8f5bd478
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-01-23 23:27:35 -0500

    Constructor Flow

---
+++ simpleClassExample/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleClassExample
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+        public Car()
+        {
+            petName = "Chuck Noris"; currSpeed = 10;
+        }
+        public void printState()
+        {
+            Console.WriteLine($"{petName} is going {currSpeed} MPH");
+        }
+        public void speedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
+
