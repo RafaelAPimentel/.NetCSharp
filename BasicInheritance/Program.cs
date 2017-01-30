52cfa22d0d2adf1958d6ef37b014a7061d46891c
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-01-29 22:46:14 -0500

    More Examples

---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(80);
+
+            myCar.Speed = 50;
+
+            Console.WriteLine("My car is going {0} MPH",myCar.Speed);
+            Console.ReadKey();
+        }
+    }
+}
