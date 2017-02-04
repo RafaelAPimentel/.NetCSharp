9fe7d019ee14aac488719b3163dd1a8cececa1aa
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-02-04 00:47:29 -0500

    End of Polymorphism

---
+++ Chapter 6/ObjectOverrides/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string SNN { get; set; } = "";
+        public int Age { get; set; }
+
+        public Person(string fname, string lname, int personAge)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Age = personAge;
+        }
+        public Person() { }
+        public override int GetHashCode()
+        {
+            return SNN.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string myString;
+            myString = string.Format("[First Name: {0}; Last Name{1}; Age{2}]", FirstName, LastName, Age);
+            return myString;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.ToString() == this.ToString();
+        }
+
+
+
+
+    }
+}
---
+++ Chapter 6/ObjectOverrides/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Homer","Simpson", 50);
+            Person p2 = new Person("Homer","Simpson", 50);
+
+            Console.WriteLine("p1.ToString() = {0}",p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+
+            Console.WriteLine("p1 = p2?: {0}", p1.Equals(p2));
+
+            Console.WriteLine("Same hash codes?: {0}",p1.GetHashCode() == p2.GetHashCode());
+            Console.WriteLine();
+            StaticMembersOfObject();
+            Console.Read();
+
+        }
+        public static void StaticMembersOfObject()
+        {
+            Person p3 = new Person("Sally", "Jones", 4);
+            Person p4 = new Person("Sally", "Jones", 4);
+
+            Console.WriteLine("Same state: {0}", object.Equals(p3, p4));
+            Console.WriteLine("same object: {0}", object.ReferenceEquals(p3, p4));
+        }
+    }
+}
