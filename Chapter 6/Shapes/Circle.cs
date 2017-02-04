55c55fec88a0060f280fcf5e486cd687fe5ebc8e
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-02-04 00:04:49 -0500

    more

---
+++ Chapter 6/Shapes/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Circle : Shapes
+    {
+
+        public Circle()
+        {
+
+        }
+        public Circle(string name)
+        :base(name){ }
+
+        public override void draw()
+        {
+            Console.WriteLine("Drawing {0} the circle", PetName);
+        }
+
+
+    }
+}
