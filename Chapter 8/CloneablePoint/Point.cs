24daf77b8a3fae2cdd3feca162e64435c247ef45
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-02-12 00:22:41 -0500

    Interfaces

---
+++ Chapter 8/CloneablePoint/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PointDescription desc = new PointDescription();
+
+        public Point() { }
+        public Point(int xPos, int yPos,string petName) { X = xPos; Y = yPos;desc.PetName = petName; }
+
+        public override string ToString()
+        {
+            return string.Format("X = {0}; Y = {1}; Name = {2};\nID = {3}", X, Y,desc.PetName,desc.PointID);
+        }
+        public object Clone() { Point newPoint = (Point)this.MemberwiseClone();
+            PointDescription currDesc = new PointDescription();
+            currDesc.PetName = this.desc.PetName;
+            newPoint.desc = currDesc;
+            return newPoint;
+        }
+    }
+}
---
+++ Chapter 8/CloneablePoint/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object passed by reference
+            Console.WriteLine("****Fun with Object Cloning****");
+            Point p1 = new Point(50, 50,"Bob");
+            Point p2 = p1;
+
+            p2.X = 0;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine();
+
+            //using the clone method 
+            Point p3 = new Point(100, 100,"Jane");
+            Point p4 = (Point)p3.Clone();
+            Console.WriteLine("Cloned p3 and stored it in p4\nBefore modification:");
+            Console.WriteLine(p3 + "\n");
+            Console.WriteLine(p4 + "\n");
+            p4.desc.PetName = "My New Point";
+            p4.X = 0;
+            Console.WriteLine("Changed p4.desc.PetName and p4.X \nAfter Modification:");
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+            
+            Console.Read();
+        }
+    }
+}
