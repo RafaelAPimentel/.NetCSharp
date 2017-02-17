2f90286ad063b07179d55bcc6c9196bcf516f353
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-02-17 00:03:55 -0500

    Finishing  Collections & Generics

---
+++ Chapter 9/FunWithObservableCollection/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithObservableCollection
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person() { }
+        public Person(string firstname, string lastname, int age) {
+            FirstName = firstname;
+            LastName = lastname;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"FirstName = {FirstName} ,LastName = {LastName}, Age = {Age}");
+        }
+    }
+}
---
+++ Chapter 9/FunWithObservableCollection/PersonCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithObservableCollection
+{
+    class PersonCollection : IEnumerable<Person>
+    {
+        List<Person> arPeople = new List<Person>();
+
+        public Person GetPerson(int pos) {
+            return (Person)arPeople[pos];
+        }
+        public void Add(Person p) {
+            arPeople.Add(p);
+        }
+
+        public void ClearPeople() {
+             arPeople.Clear();
+        }
+
+        public int Count {
+            get { return arPeople.Count; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return arPeople.GetEnumerator();
+        }
+
+        IEnumerator<Person> IEnumerable<Person>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
