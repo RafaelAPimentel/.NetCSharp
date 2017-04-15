7a18a3f9725369880781f812f653e7735344a8e8
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-04-15 01:36:45 -0400

    Examples of Interface name clashing and hierachy

---
+++ Chapter 8/InterfaceHierarchy/InterfaceHierarchy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Simple Interface Hierarchy ****");
+
+            BitMapImage myBitImage = new BitMapImage();
+
+            //callin methods from object levlel
+            myBitImage.draw();
+            myBitImage.DrawInBoundingBox(10,10,100,150);
+            myBitImage.DrawUpsideDown();
+
+            //CAlling from IAdvancedDraw Explicitly
+            IAdvancedDraw iAdvDraw = myBitImage as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+
+            Console.ReadLine();
+
+        }
+    }
+}
