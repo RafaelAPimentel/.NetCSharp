67614851832d7422cbb3624656324dd5effb87b9
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-04-20 22:30:36 -0400

    Added more description

--- Chapter 8/ComparableCar/ComparableCar/Program.cs
+++ Chapter 8/ComparableCar/ComparableCar/Program.cs
@@ -17,16 +17,16 @@ namespace ComparableCar
             myAutos[2] = new Car("Viper", 40, 34);
             myAutos[3] = new Car("Mel", 40, 4);
             myAutos[4] = new Car("Chcky", 40, 5);
-
+            Console.WriteLine("Unordered array");
             foreach (Car c in myAutos)
                 Console.WriteLine($"{c.CarID} : {c.PetName}");
             Array.Sort(myAutos);
-            Console.WriteLine();
+            Console.WriteLine("\nSorted");
             foreach (Car c in myAutos)
                 Console.WriteLine($"{c.CarID } : { c.PetName}");
 
             Array.Sort(myAutos, Car.SortByPetName);
-            Console.WriteLine();
+            Console.WriteLine("\nSorted by PetName");
             foreach (Car c in myAutos)
             {
                 Console.WriteLine($"{c.CarID} : {c.PetName }");
