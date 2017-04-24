339f3bea9ae476ea375989a8f952120b147a511c
rafael.pimentel <rafael.pimentel@USB-L1727>
2017-04-23 21:56:16 -0400

    Created a MyGenericClass that have static generic methods to swap and display basetype

--- Chapter 9/CustomGenericMethods/CustomGenericMethods/Program.cs
+++ Chapter 9/CustomGenericMethods/CustomGenericMethods/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace CustomGenericMethods
 {
@@ -15,24 +15,22 @@ namespace CustomGenericMethods
             //swap 2 ints
             int a = 10, b = 20;
             Console.WriteLine($"Before swap: {a}:{b}");
-            Swap<int>(ref a, ref b);
+            MyGenericMethods.Swap<int>(ref a, ref b);
             Console.WriteLine($"After swap: {a}:{b}");
             Console.WriteLine();
 
             //swap 2 strings
             string s1 = "Hello", s2 = "There";
             Console.WriteLine($"Before swap: {s1}:{s2}");
-            Swap<string>(ref s1,ref s2);
-            Console.WriteLine($"After swap: {s1}:{s2}");
+            MyGenericMethods.Swap<string>(ref s1,ref s2);
+            Console.WriteLine($"After swap: {s1}:{s2}\n");
+            MyGenericMethods.DisplayBaseClass<string>();
+            MyGenericMethods.DisplayBaseClass<int>();
+            MyGenericMethods.DisplayBaseClass<object>();
+            
             Console.ReadKey();
         }
-        static void Swap<T>(ref T  a, ref T b) {
-            Console.WriteLine($"You sent the Swap() method a {typeof(T)}");
-            T temp;
-            temp = a;
-            a = b;
-            b = temp;
-        }
+
 
     }
 }
