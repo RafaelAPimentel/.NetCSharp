5156b317e4dffc90cdc6456f65f13c615253fa9a
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-04-23 22:56:46 -0400

    cleaning

--- Chapter 9/FunWithObservableCollection/FunWithObservableCollection/Person.cs
+++ Chapter 9/FunWithObservableCollection/FunWithObservableCollection/Person.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FunWithObservableCollection
+﻿namespace FunWithObservableCollection
 {
     class Person
     {
         public int Age { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Person()
-        {
-
-        }
+        public Person(){}
         public Person(string firstname, string lastname, int age)
         {
             FirstName = firstname;
