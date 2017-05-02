f46460bd7ed8229e08b9bdf473baeb534d16c4de
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-05-01 22:45:27 -0400

    Added Generic Delegate

---
+++ Chapter 10/GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDelegate
+{
+    class Program
+    {
+        //Generic delegate can represent any mehod
+        //returning void and taking a single parameter of type T
+        public delegate void MyGenericDelegate<T>(T arg);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Generic Delegates****");
+
+            //register targets
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("Some string data\n");
+
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            intTarget(9);
+            Console.ReadKey();
+        }
+
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine($"arg in uppercase is: {arg.ToUpper()}");
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine($"++arg in uppercase is :{++arg}");
+        }
+    }
+}
