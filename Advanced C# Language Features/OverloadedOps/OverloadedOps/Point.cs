7e50d4908a4ab1620ef986bab226a44aaccde994
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-05-07 23:51:30 -0400

    Overloading operators with a point class that has two field members

---
+++ Advanced C# Language Features/OverloadedOps/OverloadedOps/Point.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format($"[{X},{Y}]");
+        }
+        public static Point operator +(Point pt1, Point pt2)
+        {
+            return new Point(pt1.X + pt2.X, pt1.Y + pt2.Y);
+        }
+        public static Point operator -(Point pt1, Point pt2)
+        {
+            return new Point(pt1.X - pt2.X, pt1.Y - pt2.Y);
+        }
+        public static Point operator +(Point pt1, int change)
+        {
+            return new Point(pt1.X + change, pt1.Y + change);
+        }
+        public static Point operator +(int change, Point pt1)
+        {
+            return new Point(pt1.X + change, pt1.Y + change);
+        }
+        public static Point operator ++(Point p1)
+        {
+            return new Point(p1.X++, p1.Y++);
+        }
+        public static Point operator --(Point p1)
+        {
+            return new Point(p1.X--, p1.Y--);
+        }
+        public override bool Equals(object obj)
+        {
+            return this.ToString() == obj.ToString();
+        }
+
+        public static bool operator ==(Point p1, Point p2)
+        {
+            return p1.Equals(p2);
+        }
+        public static bool operator !=(Point p1, Point p2)
+        {
+            return !p1.Equals(p2);
+        }
+        public static bool operator <(Point p1, Point p2)
+        {
+            return p1.CompareTo(p2) < 0;
+        }
+        public static bool operator >(Point p1, Point p2)
+        {
+            return p1.CompareTo(p2) > 0;
+        }
+        public static bool operator <=(Point p1, Point p2)
+        {
+            return p1.CompareTo(p2) <= 0;
+        }
+        public static bool operator >=(Point p1, Point p2)
+        {
+            return p1.CompareTo(p2) >= 0;
+        }
+        public int CompareTo(Point other)
+        {
+            if (this.X > other.X && this.Y > other.Y)
+                return 1;
+            if (this.X < other.X && this.Y < other.Y)
+                return -1;
+            else
+                return 0;
+        }
+
+    }
+
+}
---
+++ Advanced C# Language Features/OverloadedOps/OverloadedOps/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Overloaded Operators ****");
+
+            //Make two points
+            Point ptOne = new Point(100, 100);
+            Point ptTwo = new Point(40, 40);
+            Console.WriteLine($"ptOne = {ptOne}");
+            Console.WriteLine($"ptTwo = {ptTwo}");
+
+            //add both point classes
+            Console.WriteLine($"ptOne + ptTwo = {ptOne+ptTwo}");
+
+            //prints [110,110]
+            Point BiggerPoint = ptOne + 10;
+            Console.WriteLine($"Bigger point = {BiggerPoint}");
+
+            //prints [120,120]
+            Console.WriteLine($"Biggest point = { BiggerPoint+10} ");
+            Console.WriteLine("APPLYING THE ++ AND -- UNARY OPERATORS TO A POINT");
+            Point ptFive = new Point(1, 1);
+            Console.WriteLine($"++ptFive = {++ptFive}");
+            Console.WriteLine($"--ptFive = {--ptFive}");
+            Point ptSix = new Point(20, 20);
+            Console.WriteLine($"ptSix++ = {ptSix++}");
+            Console.WriteLine($"ptSix-- = {ptSix--}\n");
+            Console.WriteLine($"ptOne == ptTwo: {ptOne==ptTwo}");
+            Console.WriteLine($"ptOne == ptTwo: {ptOne != ptTwo}");
+            Console.Read();
+        }
+    }
+}
