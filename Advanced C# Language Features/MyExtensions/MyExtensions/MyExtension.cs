fb555237b5226b22192b74f0e4bd0b3beebd46e3
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-05-09 23:28:02 -0400

    Added Extension Methods

---
+++ Advanced C# Language Features/MyExtensions/MyExtensions/MyExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    static class MyExtension
+    {
+        //this method allows an object to display its assembly
+        //it is  defined in
+        public static void DisplayDefiningAssembly(this object obj) {
+            Console.WriteLine($"{obj.GetType().Name} lives here: => {Assembly.GetAssembly(obj.GetType()).GetName().Name}");
+        }
+
+        public static int ReverseDigit(this int i ) {
+            char[] digits = i.ToString().ToCharArray();
+
+            Array.Reverse(digits);
+
+            string newDigits = new string(digits);
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ Advanced C# Language Features/MyExtensions/MyExtensions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Extension Method****");
+
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+            sp.DisplayDefiningAssembly();
+
+            Console.WriteLine($"Value of myInt: {myInt}");
+            Console.WriteLine($"Reversed digits of myInt: {myInt.ReverseDigit()}");
+
+            Console.Read();
+        }
+    }
+}
