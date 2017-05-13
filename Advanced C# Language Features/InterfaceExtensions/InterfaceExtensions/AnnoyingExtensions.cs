aeb9a681d04a4cd1f04af388fe7d0351e4986429
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-05-12 23:31:37 -0400

    Adding Extension methods and anonymous types

---
+++ Advanced C# Language Features/InterfaceExtensions/InterfaceExtensions/AnnoyingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtensions
+{
+    public static class AnnoyingExtensions
+    {
+        public static void PrintDataAndBeep(this IEnumerable iterator) {
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ Advanced C# Language Features/InterfaceExtensions/InterfaceExtensions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Extending Interface Compatible Type ****");
+
+            //syatem.array implements IEnumerable
+            string[] data = { "Wow", "this", "sort","of","annoying",
+                                "but","in","a", "wierd", "way", "fun"};
+            data.PrintDataAndBeep();
+
+            Console.WriteLine();
+
+            //List implements IEnumerable
+            List<int> myInt = new List<int> { 10, 15, 20 };
+            myInt.PrintDataAndBeep();
+            Console.Read();
+        }
+    }
+}
