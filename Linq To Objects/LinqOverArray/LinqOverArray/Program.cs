02a47aa72dbcbf47181f68f6db7fbc8875dd38eb
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-05-14 23:14:03 -0400

    Started learning about linqToObject

---
+++ Linq To Objects/LinqOverArray/LinqOverArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOverArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryOverStrings();
+            Console.Read();
+        }
+
+        static void QueryOverStrings() {
+            string[] currentVideoGames = { "Marrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            IEnumerable<string> subset = from g in currentVideoGames
+                                         where g.Contains(" ")
+                                         orderby g
+                                         select g;
+            foreach (var item in subset) {
+                Console.WriteLine("Games that contain a space");
+                Console.WriteLine($"Item: {item}");
+            }
+            
+        }
+    }
+}
