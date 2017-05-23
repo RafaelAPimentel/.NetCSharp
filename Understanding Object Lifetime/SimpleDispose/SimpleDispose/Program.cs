67227d35b35a9c7c2873720fe230e23a3aa205f7
rafael.pimentel <rafael.pimentel@USB-L1727>
2017-05-22 22:00:11 -0400

    Adding IDisposable and Finalizing and Garbage Collection

---
+++ Understanding Object Lifetime/SimpleDispose/SimpleDispose/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Dispose ****");
+
+            using (MyResourceWrapper rw = new MyResourceWrapper(),
+                rw2 = new MyResourceWrapper())
+            {
+                //use rw and rw2
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Understanding Object Lifetime/SimpleFinalize/SimpleFinalize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFinalize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Finalizers *****");
+            Console.WriteLine("Hit the return key to shut down this app");
+            Console.WriteLine("and force the GC to invoke Finalize()");
+            Console.WriteLine("for finalizable objects created in the AppDomain");
+            Console.Read();
+            MyResourceWrapper rw = new MyResourceWrapper();
+        }
+    }
+}
