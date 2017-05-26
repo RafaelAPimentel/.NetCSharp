cdfaf8b3e2c752b9e1b78276c6a9730ddd3a6e6c
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-05-25 22:35:25 -0400

    Learning about Client applications using custom assemblies

---
+++ Building and Configuring Class Libraries/CarLibrary/CarLibrary/CarLibrary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public enum EngineState {
+        engineAlive,engineDead
+    }
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+
+        protected EngineState engState = EngineState.engineAlive;
+        public EngineState EngineState {
+            get { return engState; }
+        }
+        public abstract void TurboBoost();
+        public Car() { }
+
+        public Car(string petName, int currentSpeed, int maxSpeed)
+        {
+            PetName = petName;
+            CurrentSpeed = currentSpeed;
+            MaxSpeed = maxSpeed;
+        }
+    }
+}
---
+++ Building and Configuring Class Libraries/VisualBasicCarClient/VisualBasicCarClient/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports CarLibrary
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("**** VB CarLibrary Client App ****")
+        Dim myMiniVan As New MiniVan()
+        myMiniVan.TurboBoost()
+
+        Dim mySportsCar As New SportsCar()
+        mySportsCar.TurboBoost()
+        Console.Read()
+
+    End Sub
+
+End Module
