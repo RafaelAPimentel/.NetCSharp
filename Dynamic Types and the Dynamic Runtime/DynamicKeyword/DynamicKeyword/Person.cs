27b8979d6caf18130049e0b006c15481844865cd
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2017-06-02 22:55:56 -0400

    readding files from old folder

---
+++ Dynamic Types and the Dynamic Runtime/DynamicKeyword/DynamicKeyword/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace DynamicKeyword
+{
+    internal class Person
+    {
+        public Person()
+        {
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
