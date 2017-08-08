fb7e1ab4b4a9b6d8535c93d4746c392683062206
rafyr <rafyr@GUIDO>
2017-08-07 22:38:44 -0400

    Learning about StringWriter / StringReader

---
+++ File IO And Object Serialization/StringReaderWriterApp/StringReaderWriterApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReaderWriterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with StringWriter / StringReader*****\n");
+
+            //Create string Writer and emit data to memory
+            using (StringWriter strWriter = new StringWriter())
+            {
+                strWriter.WriteLine("Don't forget Mother's day this year...");
+                Console.WriteLine($"Content from StringWriter:\n{strWriter}");
+
+                using (StringReader strReader = new StringReader(strWriter.ToString()))
+                {
+                    string input = null;
+                    while ((input = strReader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(input);
+                    }                    
+                }
+            }
+            Console.Read();
+        }
+    }
+}
