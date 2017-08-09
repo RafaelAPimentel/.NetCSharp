bda82c5fca2a179125ad4089994e6e84063a5fe1
rafyr <rafyr@GUIDO>
2017-08-08 22:28:39 -0400

    Learning about BinaryWriter and directory watcher

---
+++ File IO And Object Serialization/BinaryWriterReader/BinaryWriterReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryWriterReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Binary Writer / Reader ****");
+
+            //Open a binary writer for a file
+            FileInfo f = new FileInfo("BinFile.dat");
+            using (BinaryWriter bw = new BinaryWriter(f.OpenWrite()))
+            {
+                //Print out the type of BaseStream
+                Console.WriteLine($"Base stream is: {bw.BaseStream}");
+
+                //Create some data to save in the file
+                double aDouble = 1234.67;
+                int anInt = 34567;
+                string aString = "A, B, C";
+
+                //write the data
+                bw.Write(aDouble);
+                bw.Write(anInt);
+                bw.Write(aString);
+            }
+
+            using (BinaryReader br = new BinaryReader(f.OpenRead()))
+            {
+                Console.WriteLine($"Directory: {f.FullName}");
+                Console.WriteLine(br.ReadDouble());
+                Console.WriteLine(br.ReadInt32());
+                Console.WriteLine(br.ReadString());
+            }
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ File IO And Object Serialization/MyDirectoryWatcher/MyDirectoryWatcher/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDirectoryWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** The Amazing File Watcher App ****\n");
+            //Establish the path to the directory to watch 
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            try
+            {
+                watcher.Path = @".\MyFolder";
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            //Set up the things to be on the lookout for
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+                | NotifyFilters.LastWrite
+                | NotifyFilters.FileName
+                | NotifyFilters.DirectoryName;
+
+            //only watch text files
+            watcher.Filter = "*.txt";
+
+            //Add eventhandlers
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            //bBegin watching the directory
+            watcher.EnableRaisingEvents = true;
+
+            //Wait for the user to quit the program
+            Console.WriteLine(@"Press 'q' to quit app.");
+            while (Console.Read() != 'q')
+                ;
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"File: {e.OldFullPath} renamed to  {e.FullPath}");
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+        }
+    }
+}
