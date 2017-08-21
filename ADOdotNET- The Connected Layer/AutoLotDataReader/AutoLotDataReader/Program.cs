7821143bf3a00c755eb919ffa36d5b7b26b67f26
rafyr <rafyr@GUIDO>
2017-08-20 23:42:57 -0400

    Learning about connectionStringBuilder and reading multiple result sets from DataReader

---
+++ ADOdotNET- The Connected Layer/AutoLotDataReader/AutoLotDataReader/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace AutoLotDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("**** Fun with Data Readers ****");
+
+            //Create a connection string via the builder object
+            var cnStringBuilder = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "AutoLot",
+                DataSource = @"(local)\SQLEXPRESS",
+                ConnectTimeout = 30,
+                IntegratedSecurity = true
+            };
+
+            //Create and open a conneciton
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = cnStringBuilder.ConnectionString;
+                connection.Open();
+
+                //New  helper function (see below)
+                ShowConnectionStatus(connection);
+
+                //Create a SQL command object
+                string sql = "SELECT * FROM Inventory;SELECT * FROM Customers";
+                SqlCommand myCommand = new SqlCommand(sql, connection);
+
+                //Obtain a data reader a la ExecuteReader()
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    do
+                    {
+                        //Loop over the result
+                        while (myDataReader.Read())
+                        {
+                            WriteLine("**** Record ****");
+                            for (int i = 0; i < myDataReader.FieldCount; i++)
+                            {
+                                WriteLine($"{myDataReader.GetName(i)} = {myDataReader.GetValue(i)}");
+                            }
+                            WriteLine();
+                        }
+                    } while (myDataReader.NextResult());
+                }
+            }
+            Console.Read();
+        }
+
+        private static void ShowConnectionStatus(SqlConnection connection)
+        {
+            //Show various stats about current connection object
+            WriteLine("**** Info about your connection ****");
+            WriteLine($"Database locaiton: {connection.DataSource}");
+            WriteLine($"Database name: {connection.Database}");
+            WriteLine($"Timeout: {connection.ConnectionTimeout}");
+            WriteLine($"Connection state: {connection.State}\n");
+        }
+    }
+}
