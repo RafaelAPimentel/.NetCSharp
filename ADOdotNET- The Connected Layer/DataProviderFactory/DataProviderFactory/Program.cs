b8de3948cbdad6c52e0d9ea7026790b2b76ba797
rafyr <rafyr@GUIDO>
2017-08-20 22:47:39 -0400

    Learning about ADO.NET Using connection strings in config app

---
+++ ADOdotNET- The Connected Layer/DataProviderFactory/DataProviderFactory/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DataProviderFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("**** Fun with Data Provider Factories \n****");
+            //Get connection string/provider from *.config
+            string dataProvider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.ConnectionStrings["AutoLotSQLProvider"].ConnectionString;
+
+            //Get the factory provider.
+            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
+
+            //Now get the connection object.
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    ShowError("Connection");
+                    return;
+                }
+                WriteLine($"Your connection object is a: {connection.GetType().Name}");
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                //Make command object
+                DbCommand command = factory.CreateCommand();
+                if (command == null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+                WriteLine($"Your command object is a: {command.GetType()}");
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Inventory";
+
+                //Print out data with data reader
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    WriteLine($"Your data reader object is a: {dataReader.GetType().Name}");
+
+                    WriteLine("\n**** Current Inventory ****");
+                    while (dataReader.Read())
+                    {
+                        WriteLine($"-> Car #{dataReader["CarID"]} is a {dataReader["Make"]}");
+                    }
+                }
+                ReadLine();
+            }
+        }
+
+        private static void ShowError(string objectName)
+        {
+            WriteLine($"There was an issue creating the {objectName}");
+            ReadLine();
+        }
+    }
+}
---
+++ ADOdotNET- The Connected Layer/MyConnectionFactory/MyConnectionFactory/Program.cs
@@ -0,0 +1,65 @@
+﻿using static System.Console;
+
+using System.Data;
+using System;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Configuration;
+
+namespace MyConnectionFactory
+{
+    class Program
+    {
+        //  A list of possible providers
+        enum DataProvider
+        {
+            SqlServer, OleDB, Odbc, None
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("**** Very Simple Connection  Factory ****");
+            //Read the provider key
+            string dataProviderString = ConfigurationManager.AppSettings["provider"];
+            //Transform string to enum
+            DataProvider dataProvider = DataProvider.None;
+            if (Enum.IsDefined(typeof(DataProvider), dataProviderString))
+            {
+                dataProvider = (DataProvider)Enum.Parse(typeof(DataProvider), dataProviderString);
+            }
+            else
+            {
+                WriteLine("Sorry, no provider exists!");
+                ReadLine();
+                return;
+            }
+            //Get a specific connection 
+            IDbConnection myConnection = GetConnection(dataProvider);
+            WriteLine($"Your connection is a {myConnection.GetType().Name}");
+
+            //Open use and close connection 
+            ReadLine();
+        }
+
+        //This method returns a specific connection object
+        //based on the value of a DataProvider enum
+        private static IDbConnection GetConnection(DataProvider dataProvider)
+        {
+            IDbConnection connection = null;
+            switch (dataProvider)
+            {
+                case DataProvider.SqlServer:
+                    connection = new SqlConnection();
+                    break;
+                case DataProvider.OleDB:
+                    connection = new OleDbConnection();
+                    break;
+                case DataProvider.Odbc:
+                    connection = new OdbcConnection();
+                    break;
+            }
+            return connection;
+        }
+    }
+}
