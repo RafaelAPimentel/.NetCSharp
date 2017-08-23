7c6333769855af890d2a3d6b1ba13128821bd083
rafyr <rafyr@GUIDO>
2017-08-22 23:56:37 -0400

    Created a CUI to interact with database

---
+++ ADOdotNET- The Connected Layer/AutoLotCUIClient/AutoLotCUIClient/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL;
+using static System.Console;
+using System.Configuration;
+using AutoLotDAL.ConnectedLayer;
+using System.Data;
+using AutoLotDAL.Models;
+
+namespace AutoLotCUIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("**** The AutoLot Console UI ****");
+
+            //Get connectionstring form app.config
+            string connectionstring = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
+            bool userDone = false;
+            string userCommand = string.Empty;
+
+            //Create out Inventory object
+            InventoryDAL invDal = new InventoryDAL();
+            invDal.OpenConnection(connectionstring);
+
+            //keep asking for input until user presses Q key
+            try
+            {
+                ShowInstructions();
+                do
+                {
+                    WriteLine("\nPlease enter your command");
+                    userCommand = ReadLine();
+                    switch (userCommand.ToUpper()??"")
+                    {
+                        case "I":
+                            InsertNewCar(invDal);
+                            break;
+                        case "U":
+                            UpdateCarPetName(invDal);
+                            break;
+                        case "D":
+                            DeleteCar(invDal);
+                            break;
+                        case "L":
+                            ListInventory(invDal);
+                            break;
+                        case "S":
+                            ShowInstructions();
+                            break;
+                        case "P":
+                            LookUpPetName(invDal);
+                            break;
+                        case "Q":
+                            userDone = true;
+                            break;
+                        default:
+                            WriteLine("Bad data! Try again!");
+                            break;
+                    }
+                } while (!userDone);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                throw ex;
+            }
+            finally
+            {
+                invDal.CloseConnection();
+            }
+        }
+
+        private static void LookUpPetName(InventoryDAL invDal)
+        {
+            //Get id of the car to look up..
+            Write("Enter ID of car to look up: ");
+            int id = int.Parse(ReadLine() ?? "0");
+            Write($"Petname of {id} is {invDal.LookUpPetName(id).TrimEnd()}.");
+        }
+
+        private static void ListInventory(InventoryDAL invDal)
+        {
+            //Get the list of inventory
+            DataTable dt = invDal.GetAllInventoryAsDataTable();
+            //Pass datatable to helper function to display
+            DisplayTable(dt);
+        }
+
+        private static void DisplayTable(DataTable dt)
+        {
+            //Print out the column names.
+            for (int curCol = 0; curCol < dt.Columns.Count; curCol++)
+            {
+                Write($"{dt.Columns[curCol].ColumnName}\t");
+            }
+            WriteLine("\n------------------------------------");
+
+            //print datatable
+            for (int curRow = 0; curRow < dt.Rows.Count; curRow++)
+            {
+                for (int curCol = 0; curCol < dt.Columns.Count; curCol++)
+                {
+                    Write($"{dt.Rows[curRow][curCol]}\t");
+                }
+                WriteLine();
+            }
+        }
+
+        private static void DeleteCar(InventoryDAL invDal)
+        {
+            //Get ID of Car to delete
+            Write("Enter ID of Car to delete: ");
+            int id = int.Parse(ReadLine() ?? "0");
+
+            //Just in case you have a referential integrity violation
+            try
+            {
+                invDal.DeleteCar(id);
+                WriteLine("Car has been deleted");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+
+        private static void UpdateCarPetName(InventoryDAL invDal)
+        {
+            //First get the user data
+            Write("Enter Car ID: ");
+            var carID = int.Parse(ReadLine() ?? "0");
+            Write("Enter New Pet Name: ");
+            var newCarPetName = ReadLine();
+
+            //Now pass to the data access library
+            invDal.UpdateCarPetName(carID, newCarPetName);
+            Write("Cars name has been changed");
+        }
+
+        private static void InsertNewCar(InventoryDAL invDal)
+        {
+            Write("Enter Car ID: ");
+            var newCarID = int.Parse(ReadLine()??"0");
+            Write("Enter Car Color: ");
+            var newCarColor = ReadLine();
+            Write("Enter Car Make: ");
+            var newCarMake = ReadLine();
+            Write("Enter Pet Name: ");
+            var newCarPetName = ReadLine();
+
+            //the insert method takes on a car class 
+            var c = new NewCar
+            {
+                CarId = newCarID,
+                Color = newCarColor,
+                Make = newCarMake,
+                PetName = newCarPetName
+            };
+
+            //Now pass to dat access library
+            invDal.InsertAuto(c);
+            WriteLine("Car has been inserted");
+        }
+
+        private static void ShowInstructions()
+        {
+            WriteLine("I: Insert a new car.");
+            WriteLine("U: Update an existing car.");
+            WriteLine("D: Deletes and existing car.");
+            WriteLine("L: Lists current inventory.");
+            WriteLine("S: Shows these Instructions.");
+            WriteLine("P: Looks up pet name.");
+            WriteLine("Q: Quits program.");
+        }
+    }
+}
--- ADOdotNET- The Connected Layer/AutoLotDAL/AutoLotDAL/ConnectedLayer/InventoryDAL.cs
+++ ADOdotNET- The Connected Layer/AutoLotDAL/AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -29,19 +29,56 @@ namespace AutoLotDAL.ConnectedLayer
 
         public void InsertAuto(NewCar car)
         {
-            //Format and execute SQL statement
-            string sql = $"Insert into Inventory (Make, Color, PetName) VALUES ('{car.Make}','{car.Color}',{car.PetName}')";
+            //Note the placeholders in the SQL query
+            string sql = $"Insert into Inventory (Make, Color, PetName) VALUES ('{car.Make}','{car.Color}','{car.PetName}')";
 
-            //Execute using our connection
+            //This command will have internal parameter
             using (SqlCommand command = new SqlCommand(sql, _sqlconnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public string LookUpPetName(int carID)
+        {
+            string carPetName;
+
+            //Establish nae of stroed proc.
+            using (SqlCommand command = new SqlCommand("GetPetName", _sqlconnection))
             {
                 try
                 {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    //Input parameters
+                    SqlParameter parm = new SqlParameter
+                    {
+                        ParameterName = "@carID",
+                        SqlDbType = SqlDbType.Int,
+                        Value = carID,
+                        Direction = ParameterDirection.Input
+                    };
+                    command.Parameters.Add(parm);
+
+                    parm = new SqlParameter
+                    {
+                        ParameterName = "@petName",
+                        SqlDbType = SqlDbType.Char,
+                        Size = 10,
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(parm);
+
+                    //execute the stored proc
                     command.ExecuteNonQuery();
+
+                    //return output param
+                    carPetName = (string)command.Parameters["@petName"].Value;
+                    return carPetName;
                 }
-                catch (SqlException ex)
+                catch (Exception ex)
                 {
-                    Exception error = new Exception("Couldn't Insert into Inventory table!", ex);
+                    Exception error = new Exception("Sorry couldnt look up petname!",ex);
                     throw error;
                 }
             }
@@ -49,11 +86,18 @@ namespace AutoLotDAL.ConnectedLayer
 
         public void DeleteCar(int id)
         {
-            string sql = $"DELETE FROM Inventory Where CarId = '{id}'";
+            string sql = $"DELETE FROM Inventory Where CarId = @carId";
             using (SqlCommand command = new SqlCommand(sql, _sqlconnection))
             {
                 try
                 {
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@carId",
+                        Value = id,
+                        SqlDbType = SqlDbType.Int
+                    };
+                    command.Parameters.Add(parameter);
                     command.ExecuteNonQuery();
                 }
                 catch (SqlException ex)
@@ -64,46 +108,63 @@ namespace AutoLotDAL.ConnectedLayer
             }
         }
 
-        public void UpdateCarPetName(int id,string newPetName) {
+        public void UpdateCarPetName(int id, string newPetName)
+        {
             //Update the PetName of the car with a specific CarId.
-            string sql = $"UPDATE Inventory SET PetName = '{newPetName}' WHERE CarId = '{id}'";
-            using (SqlCommand command = new SqlCommand(sql,_sqlconnection))
+            string sql = $"UPDATE Inventory SET PetName = @petName WHERE CarId = @carId";
+            using (SqlCommand command = new SqlCommand(sql, _sqlconnection))
             {
                 try
                 {
+                    SqlParameter parameter = new SqlParameter
+                    {
+                        ParameterName = "@petName",
+                        Value = newPetName,
+                        SqlDbType = SqlDbType.Char,
+                        Size = 25
+                    };
+                    command.Parameters.Add(parameter);
+
+                    parameter = new SqlParameter
+                    {
+                        ParameterName = "@carId",
+                        Value = id,
+                        SqlDbType = SqlDbType.Int
+                    };
+                    command.Parameters.Add(parameter);
                     command.ExecuteNonQuery();
                 }
                 catch (Exception ex)
                 {
-                    Exception error = new Exception("Couldn't Update Inventory table!", ex);
+                    Exception error = new Exception("Couldn't update inventory table!", ex);
                     throw error;
-                }                
+                }
             }
         }
 
-        public DataTable GetAllInventoryAsList()
+        public DataTable GetAllInventoryAsDataTable()
         {
             //This will hold the records.
             DataTable dataTable = new DataTable();
 
             //prep command object
             string sql = $"SELECT * FROM Inventory";
-            using (SqlCommand command = new SqlCommand(sql,_sqlconnection))
+            using (SqlCommand command = new SqlCommand(sql, _sqlconnection))
             {
                 try
                 {
                     SqlDataReader dataReader = command.ExecuteReader();
                     //Fill the datatable with data from the reader and clean up
-                    dataTable.Load(command.ExecuteReader());
+                    dataTable.Load(dataReader);
                     dataReader.Close();
-                    return dataTable;
                 }
                 catch (Exception ex)
                 {
-                    Exception error = new Exception("Sorry couldn't get inventory list",ex);
+                    Exception error = new Exception("Sorry couldn't get inventory list", ex);
                     throw error;
                 }
             }
+            return dataTable;
         }
 
 
