abc42a736524dfa04a1b449f90b897a6e5cabb0f
Rafael Pimentel <rafyraf08@gmail.com>
2017-11-24 00:59:19 -0500

    Some refactoring

--- Introducing Windows Communication Foundation/MagicEightBallServiceLib/MagicEightBallServiceClient/Program.cs
+++ Introducing Windows Communication Foundation/MagicEightBallServiceLib/MagicEightBallServiceClient/Program.cs
@@ -8,10 +8,11 @@ namespace MagicEightBallServiceClient
     {
         static void Main(string[] args)
         {
-           
+
+            Console.WriteLine("***** Ask the Magic 8 Ball*****\n");
             using (EightBallClient ball = new EightBallClient())
             {
-                Console.WriteLine("***** Ask the Magic 8 Ball*****\n");
+                Console.WriteLine("Your question: ");
                 string question = Console.ReadLine();
                 string answer = ball.ObtainAnswertoQuestion(question);
                 Console.WriteLine($"8-Ball says: {answer}");
