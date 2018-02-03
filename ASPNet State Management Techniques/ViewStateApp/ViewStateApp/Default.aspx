e8e7ea45e5d080e45c07308e985da81d9fb405a5
Rafael Pimentel <rafael.pimentel.ef@outlook.com>
2018-02-03 01:29:56 -0500

    Learnign about viewstate on aspnet webforms

---
+++ ASPNet State Management Techniques/ViewStateApp/ViewStateApp/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" EnableViewState="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ListBox ID="myListBox" runat="server">
+        </asp:ListBox>
+        <br />
+        <asp:Button ID="btnPostBack" runat="server" Text="Button" />
+        <br />
+        <br />
+        <asp:Button ID="btnAddToVS" runat="server" OnClick="btnAddToVS_Click" Text="Button" />
+        <asp:Label ID="lblVSValue" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ASPNet State Management Techniques/ViewStateApp/ViewStateApp/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            myListBox.Items.Add("One");
+            myListBox.Items.Add("Two");
+            myListBox.Items.Add("Three");
+            myListBox.Items.Add("Four");
+        }
+    }
+
+    protected void btnAddToVS_Click(object sender, EventArgs e)
+    {
+        ViewState["CustomViewStateItem"] = "Some user data";
+        lblVSValue.Text = (string)ViewState["CustomViewStateItem"];
+    }
+}
